e6eb4343029e917528d12577d5c563eee3ae2683
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-17 13:39:47 +0545

    feat: add pop to top

--- App.tsx
+++ App.tsx
@@ -39,6 +39,10 @@ function Details() {
         onPress={() => navigation.navigate('Home')}
       />
       <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </SafeAreaView>
   );
 }
