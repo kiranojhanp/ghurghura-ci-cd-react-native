c6f91aa87907ae758e349aeab91f0ead16c9c18a
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-18 00:20:55 +0545

    feat: add tab icons

--- src/navigation/home.tsx
+++ src/navigation/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {TABS} from '../common/constants';
 import Home from '../screens/Home';
@@ -9,7 +10,21 @@ const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName = '';
+          if (route.name === TABS.HOME) {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === TABS.FEED) {
+            iconName = focused ? 'newspaper' : 'newspaper-outline';
+          } else if (route.name === TABS.NOTIFICATIONS) {
+            iconName = focused ? 'notifications' : 'notifications-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}>
       <Tab.Screen name={TABS.HOME} component={Home} />
       <Tab.Screen name={TABS.FEED} component={Placeholder} />
       <Tab.Screen name={TABS.NOTIFICATIONS} component={Placeholder} />
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
 import {Text, View, Button} from 'react-native';
 import {SCREENS} from '../common/constants';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 function Home({navigation}: any) {
   return (
     <View className="flex-1 items-center justify-center">
@@ -13,6 +15,8 @@ function Home({navigation}: any) {
         title="Go to Settings"
         onPress={() => navigation.navigate(SCREENS.SETTINGS)}
       />
+
+      <Ionicons name="newspaper" size={30} color="tomato" />
     </View>
   );
 }
