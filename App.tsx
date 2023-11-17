4c6f59741cf567953b0c2dde12a56f9aab6c007e
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-17 16:54:18 +0545

    feat: use tab navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   NativeStackScreenProps,
   createNativeStackNavigator,
@@ -18,16 +19,13 @@ type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
 
 function Home({navigation}: HomeProps): JSX.Element {
+  const test = {itemId: 85};
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.hello}>Hello, World</Text>
       <Button
         title="Go to Details"
-        onPress={() =>
-          navigation.navigate('Details', {
-            itemId: 85,
-          })
-        }
+        onPress={() => navigation.navigate('Details', test)}
       />
     </SafeAreaView>
   );
@@ -36,20 +34,44 @@ function Home({navigation}: HomeProps): JSX.Element {
 function Details({route}: DetailsProps) {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.hello}>{route.params.otherParam}</Text>
+      <Text style={styles.hello}>
+        {route.params.otherParam ?? 'Details page'}
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+function Settings() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.hello}>Settings!</Text>
     </SafeAreaView>
   );
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
+function HomeStack(): JSX.Element {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+  );
+}
 
 function Wrapper(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{headerShown: false}}
+        />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
