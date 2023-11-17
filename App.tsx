66a97958be923c3d1b7d1b3b5fe9f296b3d874ff
Kiran Ojha <kiranojha1226@gmail.com>
2023-11-17 13:28:01 +0545

    feat: add react navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Analytics from 'appcenter-analytics';
 import {Button, SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function App(): JSX.Element {
+function Home(): JSX.Element {
+   const navigation = useNavigation();
+
   const handleEvent = async () => {
     Analytics.trackEvent('make_payment', {Internet: 'Cellular', GPS: 'on'});
   };
@@ -10,10 +14,45 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
-
       <Text style={styles.hello}>Hello, World</Text>
       <Button onPress={handleEvent} title="Crash" />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </SafeAreaView>
+  );
+}
+
+function Details() {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.hello}>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function Wrapper(): JSX.Element {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} options={{ title: "Overview" }} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -24,10 +63,10 @@ const styles = StyleSheet.create({
   },
   hello: {
     fontSize: 20,
-    color: 'red',
+    color: 'green',
     textAlign: 'center',
     margin: 10,
   },
 });
 
-export default App;
+export default Wrapper;
